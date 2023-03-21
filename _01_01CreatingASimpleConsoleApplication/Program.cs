e3258ba440eac963a17dea4b0bb5d85813a6b2fd
maxsage <maxsage@gmail.com>
2023-03-21 17:58:28 +0000

    First commit - one console application added

---
+++ _01_01CreatingASimpleConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace _01_01CreatingASimpleConsoleApplication
+{
+    /// <summary>
+    /// Summary description for Class1.
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //
+            // TODO: Add codee to start application here
+            //     static void Main(string[] args)
+            Console.WriteLine("The first app in Beginning C# Programming!");
+        }
+    }
+}
