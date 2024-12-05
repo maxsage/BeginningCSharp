b47d96cb59587c39e5d20f1f2aeb1ff968823ffc
maxsage <maxsage@gmail.com>
2024-12-05 16:21:00 +0000

    Finished chapter 10. Fixed CardLib bug

---
+++ _10_03CardLib/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using _10_02CardLib;
+
+namespace _10_03CardLib
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Deck myDeck = new Deck();
+            myDeck.Shuffle();
+            for (int i = 0; i < 52; i++)
+            {
+                Card tempCard = myDeck.GetCard(i);
+                Console.Write(tempCard.ToString());
+                if (i != 51)
+                    Console.Write(", ");
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
